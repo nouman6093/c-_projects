e0434b623f9fc43be175839aee350bb23541647b
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-02-18 15:07:09 +0500

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,33 +1,53 @@
-//binary search in an array:
 #include <iostream>
 using namespace std;
 int main(){
-  int array[] = {1,2,3,4,5};
-  int size = sizeof(array)/sizeof(array[0]);
-  
+  //inputting array size:
+  int size;
+  cout<<"enter size of array: ";
+  cin>>size;
+
+  //declaring array:
+  int array[size];
+
+  //inputting array elements:
+  for(int i = 0; i < size; i++){
+    cout<<"enter element: ";
+    cin>>array[i];
+  }
+
+  //outputting array elements:
+  for(int i = 0; i < size; i++){
+    cout<<array[i];
+  }
+  cout<<endl;
+
+  //inputting target value to find:
   int target;
-  cout<<"enter target value to find in the array: ";
+  cout<<"enter target value to find: ";
   cin>>target;
 
+  //binarry search:
   int beginning = array[0];
-  int ending = size-1;
-  bool found = false;
-  
-  //calculating middle value:
+  int ending = array[size-1];
+  int index = 0;
+  int found = false;
   for(;beginning<=ending;){
-    int middle = beginning+(ending-beginning)/2;
-    if(array[middle]==target){
-      cout<<"found at index: "<<middle;
+    int middle = (beginning+ending)/2;
+    if(array[middle] == target){
+      index = middle;
       found = true;
       break;
-    } else if(array[middle]<target){
-      beginning = middle+1;
-    } else {
-      ending = middle-1;
+    } else if (array[middle]>target){
+      ending = middle - 1;
+    } else if(array[middle<target]){
+      beginning = middle + 1;
     }
   }
 
-  if(found == false){
+  //if else:
+  if(found == true){
+    cout<<"found at index: "<<index;
+  } else if(found == false){
     cout<<"element not available";
   }
 }
