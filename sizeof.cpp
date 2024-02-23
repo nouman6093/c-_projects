f5e00f2d156e69ebb41115616037ac5919834742
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-02-23 20:26:05 +0500

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //size of fundamental data types:
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(float)<<endl;
+}
